ad9d165eb4418e6f8d137ccd747578b942d4c5ab
sankait36 <sankait36@hotmail.com>
2019-06-20 10:08:47 -0500

    Challenge complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,54 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  return runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      backgroundColor: Colors.indigo,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade700,
+        title: Text(
+          'Ask Me Anything!',
+          style: TextStyle(
+            fontFamily: 'Montserrat',
+            fontWeight: FontWeight.bold,
+            fontSize: 25.0,
+          ),
+        ),
+      ),
+      body: EightBallAppWidget(),
+    ),
+  ));
+}
+
+class EightBallAppWidget extends StatefulWidget {
+  @override
+  _EightBallAppWidgetState createState() => _EightBallAppWidgetState();
+}
+
+class _EightBallAppWidgetState extends State<EightBallAppWidget> {
+  int ballDecisionNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: FlatButton(
+            padding: EdgeInsets.all(0),
+            splashColor: Colors.transparent,
+            highlightColor: Colors.transparent,
+            child: Image.asset('images/ball$ballDecisionNumber.png'),
+            onPressed: () {
+              setState(() {
+                ballDecisionNumber = Random().nextInt(5) + 1;
+              });
+            },
+          ),
+        ),
       ),
     );
+  }
+}
